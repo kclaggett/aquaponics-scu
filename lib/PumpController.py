acb5c6c9c465cfd447bdc211cba4feaf4b594da9
kclaggett <kclaggett@proofpoint.com>
2017-03-22 01:12:13 -0700

    Add pump control and basic config module

---
+++ lib/PumpController.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+class PumpController(object):
+    
+    def __init__(self, config):
+        self.initialize(config)
+    
+    def initialize(self, config):
+        self.onCycle = config.get('pump.on_interval.seconds', 2700)
+        self.offCycle = config.get('pump.off_interval.seconds', 900)
+        self.outPin = config.get('pump.output_board_pin')
+
+        self.totalCycle = self.onCycle + self.offCycle;
+
+        if (GPIO.getmode() == None):
+            raise Exception("GPIO mode not initialized")
+        try:
+            GPIO.cleanup(self.outPin)
+        except:
+            pass # need to do for reinit, so don't bother with exceptions
+
+        GPIO.setup(self.outPin, GPIO.OUT)
+
+        self.lastChange = 0
+        self.lastState = 'off'
+
+    def runModule(self):
+        currTime = time.time()
+        
+        if self.lastState == 'on' and currTime - self.lastChange > self.onCycle:
+            GPIO.output(self.outPin, GPIO.LOW)
+            self.lastState = 'off'
+            self.lastChange = currTime
+        elif self.lastState == 'off' and currTime - self.lastChange > self.offCycle:
+            GPIO.output(self.outPin, GPIO.HIGH)
+            self.lastState = 'off'
+            self.lastChange = currTime
---
+++ lib/SimpleConfig.py
@@ -0,0 +1,44 @@
+
+class SimpleConfig(object):
+
+    def __init__(self, file_list):
+        self.generate(file_list)
+
+    def generate(self, file_list):
+        self.data = {}
+
+        for fname in file_list:
+            try: 
+                f = open(fname, 'r')
+                for line in f:
+                    line = line.strip()
+                    if not '=' in line:
+                        continue
+
+                    pieces = line.split('=', 2)
+                    key = pieces[0].strip()
+                    value = pieces[1].strip()
+                    if len(key) < 1:
+                        continue
+
+                    self.data[key] = value || ''
+            except Exception:
+                pass
+
+    def get(self, key, default=None):
+        return self.data.get(key, default)
+
+    def getInt(self, key, default=None):
+        value = self.data.get(key, default)
+        try:
+            return int(value)
+        except ValueError:
+            return default
+
+    def getFloat(self, key, default=None):
+        value = self.data.get(key, default)
+        try:
+            return float(value)
+        except ValueError:
+            return default
+
