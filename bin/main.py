62f6f09a34044868b444cddd0fa28b2a530a5c5f
Kevin Claggett <kpclaggett@gmail.com>
2017-05-08 22:22:40 -0700

    Add a pid file to prevent multiple copies running (needs sudo for now)

--- bin/main.py
+++ bin/main.py
@@ -2,6 +2,7 @@
 
 import sys
 from time import sleep
+from pid.decorator import pidfile
 import os
 sys.path.append(os.path.abspath('..'))
 
@@ -12,32 +13,37 @@ import LightController
 import PumpController
 import TemperatureController
 
-print "Starting Aquaponics System \n"
-print "\n"
+@pidfile()
+def main():
+    print "Starting Aquaponics System \n"
+    print "\n"
 
-config = SimpleConfig.SimpleConfig(['/home/pi/aquaponics-scu/config/aquaponics.cfg'])
+    config = SimpleConfig.SimpleConfig(['/home/pi/aquaponics-scu/config/aquaponics.cfg'])
 
-# Set GPIO mode
-GPIO.setmode(GPIO.BOARD)
+    # Set GPIO mode
+    GPIO.setmode(GPIO.BOARD)
 
-pumps = PumpController.PumpController(config)
-lights = LightController.LightController(config)
-temp = TemperatureController.TemperatureController(config)
+    pumps = PumpController.PumpController(config)
+    lights = LightController.LightController(config)
+    temp = TemperatureController.TemperatureController(config)
 
-print "Initialized Modules: Lights, Pumps, Temperature \n"
-print "\n"
+    print "Initialized Modules: Lights, Pumps, Temperature \n"
+    print "\n"
 
-print "Initializing Loop, press Ctrl + C to exit \n"
+    print "Initializing Loop, press Ctrl + C to exit \n"
 
-count = 0
-while 1:
-    pumps.runModule()
-    sleep(1)
-    lights.runModule()
-    sleep(1)
-    temp.runModule()
-    current_temp = temp.getTemp()
-    print "Completed loop: {} Current Temp detected: {} \n".format(count, current_temp)
-    print "Sleeping 60 seconds before next loop"
-    count += 1
-    sleep(60)
+    count = 0
+    while 1:
+        pumps.runModule()
+        sleep(1)
+        lights.runModule()
+        sleep(1)
+        temp.runModule()
+        current_temp = temp.getTemp()
+        print "Completed loop: {} Current Temp detected: {} \n".format(count, current_temp)
+        print "Sleeping 60 seconds before next loop"
+        count += 1
+        sleep(60)
+
+if __name__ == '__main__':
+    main()
